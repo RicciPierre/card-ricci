e41a9753dadc28f8e7ca14b668d45170505bd582
Ricci <ricccipierre@gmail.com>
2021-04-21 13:56:44 +0200

    update

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 
 const chalk = require ("chalk");
 const boxen = require("boxen");
-
+const terminalImage = require('terminal-image');
+ 
 const data = {
     name: chalk.black.bgMagentaBright("                            Pierre Ricci                            "),
     work: chalk.white("Web Dev Junior at BeCode.org"),
@@ -19,25 +20,29 @@ const data = {
 const NEWLINE = "\n";
 const EMPTYLINE = "";
 
-console.log(
-    chalk.redBright(
-        boxen(
-            [
-                `${data.name}`,
-                EMPTYLINE,
-                `${data.work}`,
-                EMPTYLINE,
-                `${data.labelNpm} ${data.npm}`,
-                `${data.labelGitHub} ${data.github}`,
-                `${data.labelLinkedIn} ${data.linkedin}`,
-                EMPTYLINE,
-                `${data.labelCard} ${data.npx}`,
-            ].join(NEWLINE),
-            {
-                padding: 2,
-                margin: 3,
-                borderStyle: "doubleSingle",
-            },
+(async () => {
+    const image = await terminalImage.file('me.jpg', {width: 50, height: 30});
+    console.log( image,
+        chalk.redBright(
+            boxen(
+                [
+                    `${data.name}`,
+                    EMPTYLINE,
+                    `${data.work}`,
+                    EMPTYLINE,
+                    `${data.labelNpm} ${data.npm}`,
+                    `${data.labelGitHub} ${data.github}`,
+                    `${data.labelLinkedIn} ${data.linkedin}`,
+                    EMPTYLINE,
+                    `${data.labelCard} ${data.npx}`,
+                ].join(NEWLINE),
+                {
+                    padding: 2,
+                    margin: 3,
+                    borderStyle: "doubleSingle",
+                },
+            ),
         ),
-    ),
-);
+    );
+})();
+
